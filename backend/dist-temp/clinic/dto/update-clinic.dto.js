5cc1d70e62d7a39753093403d79983add26955bb
poring86 <matheuslino86a@gmail.com>
2026-05-13 11:42:15 -0400

    chore: remove remaining temporary build outputs

--- backend/dist-temp/clinic/dto/update-clinic.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateClinicDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const create_clinic_dto_1 = require("./create-clinic.dto");
-class UpdateClinicDto extends (0, mapped_types_1.PartialType)(create_clinic_dto_1.CreateClinicDto) {
-}
-exports.UpdateClinicDto = UpdateClinicDto;
-//# sourceMappingURL=update-clinic.dto.js.map
--- backend/dist-temp/patient/patient.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { PatientDto } from './dto/patient.dto';
-import { CreatePatientDto } from './dto/create-patient.dto';
-import { UpdatePatientDto } from './dto/update-patient.dto';
-export declare class PatientService {
-    delete(id: string): Promise<boolean>;
-    findAll(clinicId?: string): Promise<PatientDto[]>;
-    create(dto: CreatePatientDto): Promise<PatientDto>;
-    update(id: string, dto: UpdatePatientDto): Promise<PatientDto | null>;
-}
